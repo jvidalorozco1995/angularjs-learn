4d844d5785e597dcedeb072cf298ec6df34f70a4
JORGE-PC <jorgevidal1995@hotmail.com>
2018-05-21 22:40:54 -0500

    commit getting started

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
- // templateUrl: './servers.component.html',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+   templateUrl: './servers.component.html',
+  selector : 'app-servers',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Server jorge';
+  serverCreated = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is : ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+     this.serverName = ( <HTMLInputElement>event.target).value;
+     console.log((<HTMLInputElement>event.target).value);
   }
 
 }
