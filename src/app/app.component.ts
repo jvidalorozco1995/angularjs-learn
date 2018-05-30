c5a303ed70b7981103a7eb11e7680b216515eca6
JORGE-PC <jorgevidal1995@hotmail.com>
2018-05-29 23:01:06 -0500

    passing data in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  loadedFeacture = 'recipe';
+
+  onNavigate(feacture: string){
+    this.loadedFeacture = feacture;
+  }
 }
